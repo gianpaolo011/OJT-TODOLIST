353ded6955e5016a923f16caee9d1b3bd613afdd
gianpaolo011 <tengcogianpaolo014@gmail.com>
2024-05-06 13:56:09 +0800

    test

--- src/UserValidation/SetTaskSchema.jsx
+++ src/UserValidation/SetTaskSchema.jsx
@@ -1,14 +1,13 @@
 import * as yup from 'yup'
 import moment from 'moment'
 
-
 export const settaskschema = yup.object().shape({
   text: yup.string().required('Description Required.'),
   end_date: yup
     .date()
     .min(
       moment().format('YYYY-MM-DD hh:mm'),
-      'Date and Time must be present or future',
+      'Date and Time must be present or futuree',
     )
     .required('Date and Time is required.'),
 })
